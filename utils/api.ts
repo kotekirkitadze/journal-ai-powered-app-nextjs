59435ef13795737531bd569dfef32983242d424e
Konstantine Kirkitadze <74476606+kotekirkitadze@users.noreply.github.com>
2024-03-03 03:48:15 +0400

    add route for journal entry update

--- utils/api.ts
+++ utils/api.ts
@@ -1,16 +1,16 @@
 const createURL = (path: string) => {
-	return window.location.origin + path;
+  return window.location.origin + path;
 };
 
 export const createNewEntry = async () => {
-	const res = await fetch(
-		new Request(createURL("/api/journal"), {
-			method: "POST",
-		}),
-	);
+  const res = await fetch(
+    new Request(createURL('/api/journal'), {
+      method: 'POST',
+    })
+  );
 
-	if (res.ok) {
-		const data = await res.json();
-		return data.data;
-	}
+  if (res.ok) {
+    const data = await res.json();
+    return data.data;
+  }
 };
